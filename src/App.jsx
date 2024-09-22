4fd379295c932e8015339ce717bdb93ec6a62110
Ranatarek22 <rt589611@gmail.com>
2024-09-22 22:06:13 +0300

    setting the evn

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+import { NavbarSimple } from "./components/navbar/NavbarSimple.jsx";
+
+function App() {
+
+  return (
+    <>
+      <NavbarSimple />
+    </>
+  );
+}
+
+export default App;
