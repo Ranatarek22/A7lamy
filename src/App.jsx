64ae724765e4f27a7494f793159dadd63b92b778
Ranatarek22 <rt589611@gmail.com>
2024-09-22 23:34:39 +0300

    HeroUpdated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { NavbarSimple } from "./components/navbar/NavbarSimple.jsx";
-import Intro from "./components/Home/Hero.jsx";
+import Hero from "./components/Home/Hero.jsx";
 import Queries from "./components/Home/Queries.jsx";
 
 function App() {
