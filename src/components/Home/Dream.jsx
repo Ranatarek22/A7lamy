6b90af3a04def7f3755d0d0ce4bb1d15723318ec
Ranatarek22 <rt589611@gmail.com>
2024-09-28 19:06:45 +0300

    newUpdates for token

--- src/components/Home/Dream.jsx
+++ src/components/Home/Dream.jsx
@@ -34,21 +34,11 @@ const Dream = () => {
           "Content-Type": "multipart/form-data",
         },
       });
-      // token = res.data.message;
 
       setToken(res.data.message);
-      // const resPay = await apiInstance.get(`/payment`, {
-      //   params: { token },
-      //   headers: {
-      //     "ngrok-skip-browser-warning": "true",
-      //   },
-      // });
-      // navigate(`/${resPay.data}`);
-      // console.log(resPay.data);
       console.log(res.data);
-
       setResponse(res.data.data);
-
+      localStorage.setItem("token", token);
       localStorage.setItem("freeTrialUsed", "true");
       setIsFreeTrialUsed(true);
     } catch (error) {
@@ -57,6 +47,9 @@ const Dream = () => {
     }
   };
   const payment = async () => {
+    let newToken = localStorage.getItem("token");
+    console.log(newToken);
+    setToken(newToken);
     try {
       const res = await apiInstance.get(`/payment`, {
         params: { token },
@@ -64,10 +57,8 @@ const Dream = () => {
           "ngrok-skip-browser-warning": "true",
         },
       });
-   
-
+      localStorage.setItem("token", token);
       window.location.href = res.data.data;
-  
     } catch (error) {
       console.error("Error fetching ", error);
     }
@@ -92,47 +83,33 @@ const Dream = () => {
             onChange={(e) => setDreamText(e.target.value)}
           />
           <div className="flex w-full justify-between py-1.5">
-            <IconButton variant="text" color="blue-gray" size="sm">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-                className="h-4 w-4"
+            <div className="flex w-full mx-0 flex-row-reverse">
+              <Button
+                size="sm"
+                className="py-2 rounded-full mt-10 w-[25%] text-center mx-auto bg-indigo-300 text-white"
+                type="submit"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
-                />
-              </svg>
-            </IconButton>
+                عرض التفسير
+              </Button>
+
+              <Button
+                size="sm"
+                className="py-2  rounded-full  mt-10 w-[25%] text-center bg-red-400 text-white"
+                onClick={payment}
+              >
+                شحن
+              </Button>
+            </div>
           </div>
         </div>
 
-        {!isFreeTrialUsed ? (
-          <div className="flex justify-center items-center gap-2">
-            <Button
-              size="sm"
-              className="py-2 px-4 rounded-full mt-10 w-1/2 text-center mx-auto bg-indigo-300 text-white"
-              type="submit"
-            >
-              عرض التفسير
-            </Button>
-          </div>
+        {/* {!isFreeTrialUsed ? (
+          <div className="flex justify-center items-center gap-2"></div>
         ) : (
           <div className="flex justify-center items-center gap-2 mt-4">
             <p>لقد استنفدت محاولتك المجانية.</p>
-            <Button
-              size="sm"
-              className="py-2 px-4 rounded-full w-[25%] text-center bg-red-400 text-white"
-              onClick={payment}
-            >
-              شحن
-            </Button>
           </div>
-        )}
+        )} */}
 
         {response && (
           <div className="mt-4 p-4 bg-white rounded shadow-md">
